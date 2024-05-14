d7c15836c03250a536ed8ea4b9bc2ac1ee456e42
Nicko Shvetsov <shvetsovnick@gmail.com>
2024-05-14 08:57:56 -0500

    complete test added

--- src/test/java/demoqaPracticeForm.java
+++ src/test/java/demoqaPracticeForm.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import java.io.File;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class demoqaPracticeForm {
 
@@ -27,7 +27,8 @@ public class demoqaPracticeForm {
         String lastName = "Usovilas";
         String email = "nickusolovich@mail.ua";
         String phone = "3071112222";
-        String birthDay = "28 May 1981";
+        String birthDay = "28 May,1981";
+        String curentAddress = "123 New Road ln";
 
 
       open("/automation-practice-form");
@@ -39,15 +40,80 @@ public class demoqaPracticeForm {
       $("#firstName").setValue(firstName);
       $("#lastName").setValue(lastName);
       $("#userEmail").setValue(email);
-//      $("#gender-radio-2").shouldBe(visible);
+
+      //Select the right radio button
+      $("#gender-radio-1 + label").click();
+      $("#gender-radio-1").shouldBe(selected);
+
+      //Enter the phone number
       $("#userNumber").setValue(phone);
+
+      //Select DOB from drop-down calendar
       $("#dateOfBirthInput").click();
-      $("#dateOfBirthInput").clear();
-
-
       $(".react-datepicker__month-select > option[value = '4']").click();
       $(".react-datepicker__year-select > option[value = '1981']").click();
       $("[aria-label='Choose Thursday, May 28th, 1981']").click();
+
+      //Select subject
+      $("#subjectsContainer").click();
+      $("#subjectsInput").setValue("Com");
+      $(byText("Computer Science")).click();
+      $("#subjectsContainer").shouldHave(text("Computer Science"));
+
+      //Select hobbies
+      $("#hobbies-checkbox-1 + label").click();
+
+      //Upload file
+      File uploadPicture = new File("C:/Users/Nicko/Desktop/testPhoto.jpg");
+      $("#uploadPicture").uploadFile(uploadPicture);
+
+      //currentAddress form set up
+        $("#currentAddress").setValue(curentAddress);
+        $("#state").click();
+        $(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $(byText("Lucknow")).click();
+
+        //click Submit button
+        $("#submit").click();
+
+        //verify the result matching entered data
+        $$("td").findBy(text("Student Name"))
+                .sibling(0)
+                .shouldHave(text(firstName + " " + lastName));
+        $$("td").findBy(text("Student Email"))
+                .sibling(0)
+                .shouldHave(text(email));
+        $$("td").findBy(text("Gender"))
+                .sibling(0)
+                .shouldHave(text("Male"));
+        $$("td").findBy(text("Mobile"))
+                .sibling(0)
+                .shouldHave(text(phone));
+        $$("td").findBy(text("Date of Birth"))
+                .sibling(0)
+                .shouldHave(text(birthDay));
+        $$("td").findBy(text("Subjects"))
+                .sibling(0)
+                .shouldHave(text("Computer Science"));
+        $$("td").findBy(text("Hobbies"))
+                .sibling(0)
+                .shouldHave(text("Sports"));
+        $$("td").findBy(text("Picture"))
+                .sibling(0)
+                .shouldHave(text("testPhoto.jpg"));
+        $$("td").findBy(text("Address"))
+                .sibling(0)
+                .shouldHave(text(curentAddress));
+        $$("td").findBy(text("State and City"))
+                .sibling(0)
+                .shouldHave(text("Uttar Pradesh Lucknow"));
+
+
+
+
+
+
 
 
 
