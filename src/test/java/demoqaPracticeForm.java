515a750767d3168e99412af76220d61267172143
Nicko Shvetsov <shvetsovnick@gmail.com>
2024-05-13 21:54:25 -0500

    Selectors added, partially compleated the test

--- src/test/java/demoqaPracticeForm.java
+++ src/test/java/demoqaPracticeForm.java
@@ -1,10 +1,12 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.lang.module.Configuration;
+
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -15,16 +17,38 @@ public class demoqaPracticeForm {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1500";
-
-
-
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
     void fillUpPracticeForm() {
-      open("https://demoqa.com/automation-practice-form");
+        Configuration.holdBrowserOpen = true;
+        String firstName = "Nickolas";
+        String lastName = "Usovilas";
+        String email = "nickusolovich@mail.ua";
+        String phone = "3071112222";
+        String birthDay = "28 May 1981";
+
+
+      open("/automation-practice-form");
+      //verify that you are on the right page
       $(".text-center").shouldHave(text("Practice Form"));
       $(".practice-form-wrapper>h5").shouldHave(text("Student Registration Form"));
+
+      //fill up the form
+      $("#firstName").setValue(firstName);
+      $("#lastName").setValue(lastName);
+      $("#userEmail").setValue(email);
+//      $("#gender-radio-2").shouldBe(visible);
+      $("#userNumber").setValue(phone);
+      $("#dateOfBirthInput").click();
+      $("#dateOfBirthInput").clear();
+
+
+      $(".react-datepicker__month-select > option[value = '4']").click();
+      $(".react-datepicker__year-select > option[value = '1981']").click();
+      $("[aria-label='Choose Thursday, May 28th, 1981']").click();
+
 
 
 
